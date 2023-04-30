e44ceacd90fe0e8c740bf94003a868658b2ad4d5
Artem Oleynikov <allacrit@yandex.ru>
2023-04-30 18:56:30 +0300

    Initial commit

---
+++ src/main/java/task1.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class task1 {
+    public static void main(String[] args) throws IOException {
+        print(babblSort(randomArr()));
+    }
+
+    public static int[] randomArr() {
+        Random rand = new Random();
+        int arr[] = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(100);
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+        return arr;
+    }
+
+    public static int[] babblSort(int arr[]) throws IOException {
+        Logger loger = Logger.getLogger(task1.class.getName());
+        FileHandler fileHandler = new FileHandler("src/main/java/logs/Task1.txt");
+        SimpleFormatter sFormatter = new SimpleFormatter();
+        fileHandler.setFormatter(sFormatter);
+        loger.addHandler(fileHandler);
+
+        int temp;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            loger.info(Arrays.toString(arr));
+        }
+        return arr;
+    }
+
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
